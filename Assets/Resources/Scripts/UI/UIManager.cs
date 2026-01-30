7cdf3f0fafed173e9d3a088e71febd1ac3a792d1
Barquest <35894984+Barquest@users.noreply.github.com>
2026-01-30 19:29:37 +0700

    add Main Menu Scene

---
+++ Assets/Resources/Scripts/UI/UIManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private Canvas canvas;
+    public virtual void Show()
+    {
+        canvas.enabled = true;
+    }
+    public virtual void Hide()
+    {
+        canvas.enabled = false;
+    }
+}
