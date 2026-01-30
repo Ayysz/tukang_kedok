dc94f465099d87a70c279558edc17ec5e3d2ed32
Barquest <35894984+Barquest@users.noreply.github.com>
2026-01-30 20:19:25 +0700

    minigames

---
+++ Assets/Resources/Scripts/Gameplay/Minigames/MinigamePlayController.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class MinigamePlayController : MonoBehaviour
+{
+    public Action OnStartMinigame;
+    public Action OnEndMinigame;
+
+    private void OnDestroy()
+    {
+        OnStartMinigame = null;
+        OnEndMinigame = null;
+    }
+    public virtual void StartMinigame()
+    {
+        OnStartMinigame?.Invoke();
+    }
+    public virtual void EndMinigame()
+    {
+        OnEndMinigame?.Invoke();
+    }
+}
